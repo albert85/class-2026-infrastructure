09d987f380ad1074cdbda48ac34f90bc90750f85
temitope <temitope.olarewaju@arnergy.com>
2026-02-22 13:54:12 +0100

    update port

--- terraform/main.tf
+++ terraform/main.tf
@@ -59,6 +59,14 @@ resource "aws_security_group" "ansible_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    description = "Custom app port 8080"
+    from_port   = 9000
+    to_port     = 9000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   # Allow all outbound traffic
   egress {
     from_port   = 0
